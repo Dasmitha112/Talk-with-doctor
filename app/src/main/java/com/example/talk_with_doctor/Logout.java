f1065d38812772aeb3658020eafc270b3c805183
Dasmitha112 <dasmithagunasekara18671@gmail.com>
2020-08-13 02:34:20 +0530

    Toolbar and navigation bar created.

---
+++ app/src/main/java/com/example/talk_with_doctor/Logout.java
@@ -0,0 +1,49 @@
+package com.example.talk_with_doctor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.talk_with_doctor.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Logout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
+        bottomNavigationView.setSelectedItemId(R.id.logout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), com.example.talk_with_doctor.MainActivity.class));
+                        finish();
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.logout:
+                        return true;
+
+
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), com.example.talk_with_doctor.Profile.class));
+                        finish();
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+
+                return false;
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/example/talk_with_doctor/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.talk_with_doctor;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        return true;
+
+                    case R.id.logout:
+                        startActivity(new Intent(getApplicationContext(), Logout.class));
+                        finish();
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), com.example.talk_with_doctor.Profile.class));
+                        finish();
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+
+                return false;
+            }
+
+        });
+    }
+
+
+
+}
