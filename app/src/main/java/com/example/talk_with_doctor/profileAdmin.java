06d2dca534d1e17b3620694d2aa02b750ab59e92
Dasmitha112 <dasmithagunasekara18671@gmail.com>
2020-09-20 17:57:48 +0530

    Data insertion completed.

--- app/src/main/java/com/example/talk_with_doctor/profileAdmin.java
+++ app/src/main/java/com/example/talk_with_doctor/profileAdmin.java
@@ -6,15 +6,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class profileAdmin extends AppCompatActivity {
+
+    EditText editTxtId, editTxtName, editTxtEmail;
+    Button btnUpdate;
+    DatabaseReference dbRef;
+    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_admin);
+
+        editTxtId = findViewById(R.id.editTxtId);
+        editTxtName = findViewById(R.id.editTxtName);
+        editTxtEmail = findViewById(R.id.editTxtEmail);
+
+        btnUpdate = findViewById(R.id.btnUpdate);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
         bottomNavigationView.setSelectedItemId(R.id.profile);
@@ -44,5 +63,30 @@ public class profileAdmin extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("Admin");
+        readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChildren()) {
+                    editTxtId.setText(dataSnapshot.child("id").getValue().toString());
+                    editTxtName.setText(dataSnapshot.child("name").getValue().toString());
+                    editTxtEmail.setText(dataSnapshot.child("email").getValue().toString());
+
+                }else
+                    Toast.makeText(getApplicationContext(), "No source to display", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
