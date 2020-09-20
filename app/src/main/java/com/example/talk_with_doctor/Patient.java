a1e6e0ca8e5735045651a89602ea393e08101153
nirasha9806 <nirasha.chamudini@gmail.com>
2020-09-20 17:58:36 +0530

    Database connected

---
+++ app/src/main/java/com/example/talk_with_doctor/Patient.java
@@ -0,0 +1,52 @@
+package com.example.talk_with_doctor;
+
+public class Patient {
+    private String name;
+    private String email;
+    private String address;
+    private String mobile;
+    private String password;
+
+    public Patient() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
