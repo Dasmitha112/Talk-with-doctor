4852be4890d901effc9f8ef067e6b71e311b19e3
Kaveena99 <kaveenadewminijayasekara@gmail.com>
2020-09-22 15:50:56 +0530

    Updated UI

--- app/src/main/java/com/example/talk_with_doctor/profilePharmacy.java
+++ app/src/main/java/com/example/talk_with_doctor/profilePharmacy.java
@@ -20,52 +20,52 @@ import com.google.firebase.database.ValueEventListener;
 
 public class profilePharmacy extends AppCompatActivity {
 
-//    Button button7;
-//    EditText id, name, mobile, address,email,city;
+    Button button5;
+    EditText ID, name, mobile, address,email,city;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_pharmacy);
 
-//        id = findViewById(R.id.id);
-//        name = findViewById(R.id.name);
-//        mobile = findViewById(R.id.mobile);
-//        address = findViewById(R.id.address);
-//        email = findViewById(R.id.email);
-//        city = findViewById(R.id.city);
+        ID = findViewById(R.id.ID);
+        name = findViewById(R.id.name);
+        mobile = findViewById(R.id.mobile);
+        address = findViewById(R.id.address);
+        email = findViewById(R.id.email);
+        city = findViewById(R.id.city);
 
-//        button7 = findViewById(R.id.button7);
-//
-//        button7 = (Button) findViewById(R.id.button7);
-//        button7.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
+        button5 = findViewById(R.id.button5);
 
-//                DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("Pharmacy").child("-MHLt0NUb6bJXeAs_W2f");
-//                readRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                        if (dataSnapshot.hasChildren()){
-//                            id.setText(dataSnapshot.child("id").getValue().toString());
-//                            name.setText(dataSnapshot.child("name").getValue().toString());
-//                            mobile.setText(dataSnapshot.child("mobile").getValue().toString());
-//                            address.setText(dataSnapshot.child("address").getValue().toString());
-//                            email.setText(dataSnapshot.child("email").getValue().toString());
-//                            city.setText(dataSnapshot.child("city").getValue().toString());
-//                        }
-//                        else
-//                            Toast.makeText(getApplicationContext(), "No source to display", Toast.LENGTH_SHORT).show();
-//
-//                    }
-//
-//                    @Override
-//                    public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//                    }
-//                });
-//            }
-//        });
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("Pharmacy").child("-MHLt0NUb6bJXeAs_W2f");
+                readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChildren()){
+                            ID.setText(dataSnapshot.child("id").getValue().toString());
+                            name.setText(dataSnapshot.child("name").getValue().toString());
+                            mobile.setText(dataSnapshot.child("mobile").getValue().toString());
+                            address.setText(dataSnapshot.child("address").getValue().toString());
+                            email.setText(dataSnapshot.child("email").getValue().toString());
+                            city.setText(dataSnapshot.child("city").getValue().toString());
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(), "No source to display", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
         bottomNavigationView.setSelectedItemId(R.id.profile);
